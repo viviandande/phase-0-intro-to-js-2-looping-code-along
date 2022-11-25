03043559e05ba2f54104012d27dc4dba13f5dd5b
+ <+>
2022-11-26 02:04:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(friends, message){
+    let messages = []
+for (const friend of friends){
+ messages.push (`Thank you, ${friend}, for the wonderful ${message} gift!`)
+}
+return messages
+}
+
+function countDown(n){
+for(let i=n;i>=0;i--){
+    console.log (i)
+}
+}
